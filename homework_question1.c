e6aa0411f66390a632266ece4471a7ceb778b9fd
Danielle Mataquel <danielle.mataquel@stonybrook.edu>
2024-04-02 17:13:13 -0400

    My first submission

--- homework_question1.c
+++ homework_question1.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+#define SIZE 32
+
+int main () {
+    char word[SIZE], piglatin[SIZE];
+    int i, j, firstVowel, len;
+
+    while (1) {
+        printf ("Enter a word: ");
+        scanf ("%s", word);
+
+        len = strlen(word);
+
+        for (i = 0; i < len; i++) {
+            if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' || (word[i] == 'y' && i > '0') || word[i] == 'A' || word[i] == 'E' || word[i] == 'O' || word[i] == 'U' || (word[i] == 'Y' && i > '0')) {
+                firstVowel = i;
+                break;
+            }
+        }
+        
+        if (i == len) {
+            printf ("No vowels found in the %s\n", word);
+            continue;
+        }
+
+        if (firstVowel == 0) {
+            strcpy(piglatin, word);
+            strcpy(piglatin, "way");
+            piglatin[0] = toupper(piglatin[0]);
+            printf("\tPig Latin: %s\n", piglatin);
+        } else {
+            j = 0;
+            piglatin[j] = toupper(word[firstVowel]);
+            for (i = firstVowel + 1; i < len; i++) {
+                piglatin[j++] = word[i];
+            }
+
+            piglatin[j++] = word[0];
+            for (i = 1; i < firstVowel; i++) {
+                piglatin[j++] = word[i];
+            }
+
+            strcat(piglatin, "ay");
+            piglatin[len+2] ='\0';
+            printf ("\tPig Latin: %s\n", piglatin);
+        }      
+      
+    }
+
+    return 0;
+}
--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
 
+int my_isdigit (int d) {
+    if ( d>= '0' && d <= '9') {
+        return 1;
+    }
+    return 0;
+}
+
+int my_isalpha (char ch) {
+    if ((ch >= 'a' && ch <= 'z') | (ch >= 'A' && ch <= 'Z')) {
+        return 1;
+    }
+    return 0;
+}
+
+char my_is_upper (char ch) {
+    if (ch >= 'A' && ch <= 'Z') {
+        return 1;
+    }
+    return 0;
+}
+
+char my_to_upper (char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        char res = ch - 'A' + 'a';
+        return res;
+    }
+    return ch;
+}
+
+char my_to_lower (char ch) {
+    if (ch >= 'A' && ch <= 'Z') {
+        char res = ch - 'a' + 'A';
+        return res;
+    }
+    return ch;
+}
+
+int main () {
+    char a = '3';
+    char b = 'R';
+    char c = 'g';
+    char d = '$';
+    printf("%d\n",my_isdigit(a)); 
+    printf("%d\n",my_isdigit(b)); 
+    printf("%d\n",my_isalpha(b)); 
+    printf("%d\n",my_isalpha(d));
+    printf("%c\n",my_is_upper(b));
+    printf("%c\n",my_is_upper(c));
+    printf("%c\n",my_to_upper(c));  
+    printf("%c\n",my_to_upper(d));  
+    printf("%c\n",my_to_lower(b));  
+    printf("%c\n",my_to_lower(c)); 
+    
+    return 0;    
+}
